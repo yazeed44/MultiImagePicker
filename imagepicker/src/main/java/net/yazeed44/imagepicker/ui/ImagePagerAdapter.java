9f9a6802d7a404ac3d6724715f3aada6131d7742
yazeed44 <g--56@hotma.com>
2015-07-20 12:04:17 +0300

    Implemented hiding toolbar and fab Although they need to be much smoother

--- imagepicker/src/main/java/net/yazeed44/imagepicker/ui/ImagePagerAdapter.java
+++ imagepicker/src/main/java/net/yazeed44/imagepicker/ui/ImagePagerAdapter.java
@@ -2,6 +2,8 @@ package net.yazeed44.imagepicker.ui;
 
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -10,6 +12,9 @@ import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
 import net.yazeed44.imagepicker.model.AlbumEntry;
 import net.yazeed44.imagepicker.model.ImageEntry;
+import net.yazeed44.imagepicker.util.Events;
+
+import de.greenrobot.event.EventBus;
 
 
 /**
@@ -40,16 +45,35 @@ public class ImagePagerAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int position) {
         final ImageEntry imageEntry = mAlbumEntry.imageList.get(position);
         final SubsamplingScaleImageView imageView = new SubsamplingScaleImageView(mContext);
-        imageView.setImage(ImageSource.uri(imageEntry.path));
+        setupTouchGesture(imageView, imageEntry);
 
         container.addView(imageView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         return imageView;
     }
+
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
         container.removeView((View) object);
     }
 
+    private void setupTouchGesture(final SubsamplingScaleImageView imageView, final ImageEntry imageEntry) {
+        final GestureDetector myGestureDetector = new GestureDetector(mContext, new MyTouchGesture());
+        imageView.setImage(ImageSource.uri(imageEntry.path));
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return myGestureDetector.onTouchEvent(event);
+            }
+        });
+    }
 
+    private static class MyTouchGesture extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            EventBus.getDefault().post(new Events.OnTapImageEvent());
+            return super.onSingleTapConfirmed(e);
+        }
+    }
 }
